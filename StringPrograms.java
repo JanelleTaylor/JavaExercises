c2aac6350857b77330cf72b0b8b126c39092dd2d
JanelleT <49959042+JanelleTaylor@users.noreply.github.com>
2024-09-17 20:39:57 -0400

    Add files via upload
    Exercise 1: Reverse the given string.

---
+++ StringPrograms.java
@@ -0,0 +1,35 @@
+package javaExercise.Ex01;
+
+//How do you reverse a string in Java?
+
+public class StringPrograms {
+    public static void main(String[] args) {
+        String str = "123";
+        /*
+            Solution
+            Summary of what steps will be taken to achieve reversing the string
+            1. Create a new empty string to hold the newly reverse string.
+            2. Use a for loop, to loop through each character in the string and re-assign that character to new string.
+            3. Print newly reversed string.
+        */
+
+        //Creating new empty string to hold reversed string.
+        String newStr = "";
+
+        /*
+            Variable to get the last position number of the string. Since position starts at zero (0), to get the
+            last position it will be length of the string minus one (1).
+         */
+        int lastCharPos = str.length() - 1;
+        //System.out.println(lastCharPos);
+
+        //Creating a for loop to cycle through each character
+        for (int i = lastCharPos; i >= 0; i--){
+            System.out.print("At position " + i + " from the original string");
+            newStr = newStr + str.charAt(i);
+            System.out.print(" we are getting character " + str.charAt(i) + " and adding it to reserved string.");
+            System.out.println(" ");
+        }
+        System.out.println(newStr);
+    }
+}
