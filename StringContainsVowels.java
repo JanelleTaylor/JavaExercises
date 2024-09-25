9e305cd0502ca0979e97cd84dfbf7328d51c13c1
JanelleT <49959042+JanelleTaylor@users.noreply.github.com>
2024-09-25 11:03:44 -0400

    Add files via upload

--- StringContainsVowels.java
+++ StringContainsVowels.java
@@ -5,19 +5,22 @@ public class StringContainsVowels {
     public static void main(String[] args) {
         String input01 = "Hello";
         String input02 = "Tv";
-
+        String input03 = "Mississippi";
         /*
             Summary of steps that we are going to take.
             1. Find the length of each string and assign to variables.
             2. Use a for loop over the characters in the string.
             3. If a character at the particular position is found it will be added to a counter.
         */
+
         int strOneLength = input01.length();
         int strTwoLength = input02.length();
+        int strThreeLength = input03.length();
 
         // Initializing a global counter to be used.
         int counterA = 0;
         int counterB = 0;
+        int counterC = 0;
 
         //Counting number of vowels in Sting 1 (input01)
         for(int i = 0; i < strOneLength; i++){
@@ -44,10 +47,19 @@ public class StringContainsVowels {
             System.out.println(input02 + " contains " + counterB + " vowels.");
         else
             System.out.println(input02 + " does not contain any vowels.");
+
+        // Alternative solution using while loop and if else shorthand
+        int j = 0;
+        while (j < strThreeLength) {
+            if (input03.charAt(j) == 'a' || input03.charAt(j) == 'e' || input03.charAt(j) == 'i' || input03.charAt(j) == 'o' || input03.charAt(j) == 'u') {
+                counterC += 1;
+                System.out.println("At position " + j + " there is an " + input03.charAt(j));
+
+            }
+            j++;
+        }
+
+        String outcome =  (counterC > 0) ? input03 + " contains " + counterC + " vowels." : " does not contain any vowels.";
+        System.out.println(outcome);
     }
-    //vowels are a, e, i, o, and u
-
-    //Code here
-
-
 }
